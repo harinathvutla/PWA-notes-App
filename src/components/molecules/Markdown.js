d017af1ebfd6931f76372f4e6dc4b958237a02b6
Harinath Vutla <harinathvutla428@gmail.com>
2020-09-30 06:24:12 +0200

    react to PWA notes app conversion

--- src/components/molecules/Markdown.js
+++ src/components/molecules/Markdown.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const Markdown = props => {
-	const input = '# This is a header\n\n And this a para';
 	return <ReactMarkdown source={props.input} />;
 };
 
