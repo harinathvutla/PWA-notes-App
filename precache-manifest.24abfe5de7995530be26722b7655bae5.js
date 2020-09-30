cdda386bdc5281fec297d3edc5f938be97425b5f
Harinath Vutla <harinathvutla428@gmail.com>
2020-09-30 07:50:16 +0200

    Updates

---
+++ precache-manifest.24abfe5de7995530be26722b7655bae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152ee315199abe32266a926e9796fa66",
+    "url": "/PWA-notes-App/index.html"
+  },
+  {
+    "revision": "d638f0d8bd4083f2782f",
+    "url": "/PWA-notes-App/static/css/main.1f170ed0.chunk.css"
+  },
+  {
+    "revision": "74e06c608c87fb5f79df",
+    "url": "/PWA-notes-App/static/js/2.bd6a903f.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/PWA-notes-App/static/js/2.bd6a903f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d638f0d8bd4083f2782f",
+    "url": "/PWA-notes-App/static/js/main.403f1868.chunk.js"
+  },
+  {
+    "revision": "f979e7f938ff82214c53",
+    "url": "/PWA-notes-App/static/js/runtime-main.dd517858.js"
+  }
+]);
